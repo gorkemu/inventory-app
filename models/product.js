48126506e1eee235d738c145aeb4ed9429a2a0a8
gorkemu <gorkemunal88@gmail.com>
2023-04-01 06:30:02 +0300

    add image property

--- models/product.js
+++ models/product.js
@@ -8,6 +8,7 @@ const ProductSchema = new Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   number_in_stock: { type: Number, required: true },
+  image: { type: String, required: true },
 });
 
 // Virtual for bookinstance's URL

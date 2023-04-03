c2aea2ba5f6ef8fea579928bc0ae7ca66613da6f
gorkemu <gorkemunal88@gmail.com>
2023-04-03 05:29:29 +0300

    increase rate limit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(helmet());
 var RateLimit = require("express-rate-limit");
 var limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
+  max: 120,
 });
 // Apply rate limiter to all requests
 app.use(limiter);

f98dbb0318f741acce4b85bf6a4567480e96106e
gorkemu <gorkemunal88@gmail.com>
2023-04-03 04:38:21 +0300

    change type to Decimal128 for price

--- models/product.js
+++ models/product.js
@@ -6,7 +6,7 @@ const ProductSchema = new Schema({
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true }, // reference to the associated category
   name: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: Schema.Types.Decimal128, required: true },
   number_in_stock: { type: Number, required: true },
   image: { type: String },
 });
